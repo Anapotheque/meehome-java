3ac0288265d85f7ba497d56dfd78990caf878980
raballand.romain@gmail.com <raballand.romain@gmail.com@f0450777-33b9-f2dc-511a-37075df8832b>
2011-11-09 14:43:46 +0000

    Mise à jour donnée

--- trunk/socket/Socket-Client/src/fr/home/socket/client/LauncherClient.java
+++ trunk/socket/Socket-Client/src/fr/home/socket/client/LauncherClient.java
@@ -1,21 +1,27 @@
 package fr.home.socket.client;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import fr.home.socket.client.model.Client;
 
 public class LauncherClient {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		Client client = new Client();
-		
-		// Tant que le Client n'est pas connecté au serveur
-		while(!client.isStop()){
-			
-			// Si on est connecté on lance le serveur
-			if(client.connection()) client.run();
-			
-			// Pause de 10 secondes entre chaques tentatives
-			Thread.sleep(10000);
+		try {
+			Client client = new Client();
+			while(!client.isStop()){
+				if(client.connection()) client.run();
+				Thread.sleep(10000);
+			}
+			client.close();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
