a806e63ce0c2243006653764020f9fefe2313298
raballand.romain@gmail.com <raballand.romain@gmail.com@f0450777-33b9-f2dc-511a-37075df8832b>
2011-11-15 14:30:08 +0000

    Correction Serveur  Socket

--- socket/Socket-Serveur/src/fr/home/socket/server/model/Client.java
+++ socket/Socket-Serveur/src/fr/home/socket/server/model/Client.java
@@ -5,45 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-import fr.home.socket.server.util.PropertiesEnum;
-import fr.home.socket.server.util.Util;
+import org.apache.log4j.Logger;
 
 public class Client implements Runnable {
 
-	private String login;
-	private Util util;
-	private Thread thread;
-	private Socket socket;
-	private BufferedReader bufferedReader;
-	
-	public Client(Socket socket) throws IOException {
-		util = new Util();
-		
-		this.socket = socket;
-		bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		thread = new Thread(this); 
-		thread.start();
-		login = bufferedReader.readLine();
-		System.out.println(login + " s'est connecté...");
-	}
-	
-	@Override
-	public void run() {
-		try {
-			String message = bufferedReader.readLine();
-			while(!message.equals(util.getData(PropertiesEnum.STOP_SERVER))) {
-				System.out.println(message);
-				message = bufferedReader.readLine();
-	        }
-			close();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	public void close() throws IOException {
-		bufferedReader.close();
-		socket.close();
-		System.out.println(login + " s'est deconnecté...");
-	}
+    static Logger logger = Logger.getLogger(Client.class);
+
+    private Socket socket;
+
+    private BufferedReader bufferedReader;
+
+    private Thread thread;
+
+    // private static Fenetre ui;
+
+    private String login;
+
+    boolean stopClient = false;
+
+    public Client(Socket socket/* , Fenetre ui */) throws IOException {
+        // this.ui = ui;
+        this.socket = socket;
+        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        login = bufferedReader.readLine();
+
+        logger.debug("Client :: [" + login + "] Connexion");
+
+        thread = new Thread(this);
+        thread.start();
+
+        // this.ui.afficher(login + " s'est connecté...");
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!stopClient) {
+                // this.ui.afficher(message);
+                String message = bufferedReader.readLine();
+                logger.debug("run :: [" + login + "] Reception message : " + message);
+            }
+            close();
+        } catch (IOException e) {
+            logger.error("run :: " + e);
+            // ui.afficher(e.getMessage());
+        }
+    }
+
+    public void close() {
+        try {
+            bufferedReader.close();
+            socket.close();
+            logger.debug("close :: [" + login + "] Deconnexion");
+            // ui.afficher(login + " s'est deconnecté...");
+        } catch (IOException e) {
+            logger.error("close :: " + e);
+        }
+    }
 }
