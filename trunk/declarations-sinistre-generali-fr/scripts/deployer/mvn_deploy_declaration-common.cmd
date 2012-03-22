18d200173b774b3d4499fd84e3f3c909baceca0a
raballand.romain@gmail.com <raballand.romain@gmail.com@f0450777-33b9-f2dc-511a-37075df8832b>
2012-03-22 08:31:19 +0000

    suppression

--- trunk/declarations-sinistre-generali-fr/scripts/deployer/mvn_deploy_declaration-common.cmd
+++
@@ -1,9 +0,0 @@
-SET DEPLOY_CMD= -DgroupId=fr.generali.declarations-sinistre.flex -Dpackaging=swf
-SET DEPLOY_CMD= %DEPLOY_CMD% -Durl=http://maven-proxy.groupe.generali.fr/nexus/content/repositories/projects-releases  -DrepositoryId=artifacts-server
-SET DEPLOY_CMD= %DEPLOY_CMD% -Dfile=%SWF_FILE% -DartifactId=%SWF_ARTIFACT_ID% -Dversion=%SWF_VERSION% 
-
-echo "=============================================" 
-echo "==  %WF_ARTIFACT_ID% :  %SWF_VERSION%     ===" 
-echo "=============================================" 
-
-mvn org.apache.maven.plugins:maven-deploy-plugin:2.5:deploy-file %DEPLOY_CMD%
--- trunk/declarations-sinistre-generali-fr/scripts/deployer/mvn_deploy_declaration-mrh-tempeteetgrele.cmd
+++
@@ -1,5 +0,0 @@
-SET SWF_VERSION=1.1.13
-SET SWF_ARTIFACT_ID=declaration-mrh-tempeteetgrele
-SET SWF_FILE=../../%SWF_ARTIFACT_ID%/trunk/bin-debug/TempeteEtGrele.swf
-
-call mvn_deploy_declaration-common.cmd
