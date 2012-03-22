2401d4e66b99969f8d6dbe1d63f31d13b2acc15b
raballand.romain@gmail.com <raballand.romain@gmail.com@f0450777-33b9-f2dc-511a-37075df8832b>
2012-03-22 08:33:14 +0000

    suppression

--- trunk/My/ajax/target/ajax/scripts/ShowDiv.js
+++
@@ -1,6 +0,0 @@
-    function show(id_Div){
-        document.getElementById(id_Div).style.visibility = "visible";
-    }
-    function hidden(id_Div){
-        document.getElementById(id_Div).style.visibility = "hidden";
-    }
