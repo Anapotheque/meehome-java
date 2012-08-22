310e4df11b85491285a58e826c7a8e4caaa8a001
raballand.romain@gmail.com <raballand.romain@gmail.com@f0450777-33b9-f2dc-511a-37075df8832b>
2012-08-22 16:27:12 +0000

    Add data

--- SDL/sdl/src/installation/Installation.java
+++ SDL/sdl/src/installation/Installation.java
@@ -7,6 +7,10 @@ import sdljava.video.SDLVideo;
 
 public class Installation {
 	
+	private static final int WIDTH = 400;
+	private static final int HEIGHT = 200;
+	private static final int COLOR = 32;
+	
 	/**
 	 * tuto : http://www.siteduzero.com/tutoriel-3-90661-les-surfaces.html
 	 *     	SDL_DISABLE 	: cache le curseur de la souris ;
@@ -27,7 +31,7 @@ public class Installation {
 		SDLMain.init(SDLMain.SDL_INIT_VIDEO);
 		
 		// Creation de la fenetre principale
-		SDLSurface screen = SDLVideo.setVideoMode(400, 200, 32, SDLVideo.SDL_ENABLE | SDLVideo.SDL_HWSURFACE | SDLVideo.SDL_DOUBLEBUF);
+		SDLSurface screen = SDLVideo.setVideoMode(WIDTH, HEIGHT, COLOR, SDLVideo.SDL_ENABLE | SDLVideo.SDL_HWSURFACE | SDLVideo.SDL_DOUBLEBUF);
 		
 		// Definition du titre de la video
 		SDLVideo.wmSetCaption("SDL Windows", null);
