5702a08c004a11ab3928fc2bca7b8e2046b67d4f
raballand.romain@gmail.com <raballand.romain@gmail.com@f0450777-33b9-f2dc-511a-37075df8832b>
2012-08-23 15:11:21 +0000

    Add data

--- SDL/sdl/src/installation/Installation.java
+++ SDL/sdl/src/installation/Installation.java
@@ -2,13 +2,18 @@ package installation;
 
 import sdljava.SDLException;
 import sdljava.SDLMain;
+import sdljava.event.SDLEvent;
+import sdljava.event.SDLKey;
+import sdljava.event.SDLKeyboardEvent;
+import sdljava.image.SDLImage;
+import sdljava.video.SDLRect;
 import sdljava.video.SDLSurface;
 import sdljava.video.SDLVideo;
 
 public class Installation {
 	
-	private static final int WIDTH = 400;
-	private static final int HEIGHT = 200;
+	private static final int WIDTH = 600;
+	private static final int HEIGHT = 545;
 	private static final int COLOR = 32;
 	
 	/**
@@ -25,34 +30,74 @@ public class Installation {
 	 * @throws SDLException
 	 * @throws InterruptedException
 	 */
-	public Installation() throws SDLException, InterruptedException {
-		
+	
+	private SDLSurface screen = null;
+	
+	public void init() throws SDLException {
 		// Initialisation SDL Video
 		SDLMain.init(SDLMain.SDL_INIT_VIDEO);
 		
 		// Creation de la fenetre principale
-		SDLSurface screen = SDLVideo.setVideoMode(WIDTH, HEIGHT, COLOR, SDLVideo.SDL_ENABLE | SDLVideo.SDL_HWSURFACE | SDLVideo.SDL_DOUBLEBUF);
+		screen = SDLVideo.setVideoMode(WIDTH, HEIGHT, COLOR, SDLVideo.SDL_ENABLE | SDLVideo.SDL_HWSURFACE | SDLVideo.SDL_DOUBLEBUF);
 		
 		// Definition du titre de la video
-		SDLVideo.wmSetCaption("SDL Windows", null);
+		SDLVideo.wmSetCaption("My SDL", null);
+	}
+	
+	public Installation() throws SDLException, InterruptedException {
 		
-		// Changement de la couleur de fond de la fenetre
-		long blue = screen.mapRGB(0, 0, 255);
-		long red = screen.mapRGB(255, 0, 0);
-		long green = screen.mapRGB(0, 255, 0);
+		init();
 		
-		screen.fillRect(blue);
+		// Gestion des evenements 1/2
+		SDLEvent event = null;
+		boolean running = true;
 		
-		// On crée un nouveau rectangle
-		SDLSurface rectangle = SDLVideo.createRGBSurface(SDLVideo.SDL_HWSURFACE, 200, 150, 32, 0, 0, 0, 0);
-		// On attache le rectangle sur la fenêtre (qui est une SDLSurface du doux nom de screen)
-		rectangle.blitSurface(screen);
+		SDLSurface image = SDLImage.load("images/font.jpg");
+		SDLRect imagePos = new SDLRect(0, 0);
 		
-		// On affiche le tout ! C'est la fenêtre qui contient la surface rectangle, donc c'est elle qu'on flip
-		screen.flip();
+		SDLSurface tank = SDLImage.load("images/tank.png");
+		tank.setColorKey(SDLVideo.SDL_SRCCOLORKEY, tank.mapRGB(255, 0, 255));
+		SDLRect tankPos = new SDLRect(100, 100);
 		
-		//////////////////////////////////////////////////
-		Thread.sleep(2000);
+		// Gestion de la transparence
+		tank.setAlpha(SDLVideo.SDL_SRCALPHA, 200);
+		
+		/*
+		 *  Gestion des événement 2/2
+		 *  
+		 *  Boucle Principale
+		 *  
+		 *  Le code est exécuté TANT QUE la variable booléene boucle est vrai
+		 *  SI l'événement SDL_QUIT est vrai ALORS la variable boucle est fausse et donc on sort de la boucle
+		 */
+		while (running) {
+			event = SDLEvent.waitEvent();
+			
+			if (event.getType() == SDLEvent.SDL_QUIT){
+				running = false;
+			}
+			if (event.getType() == SDLEvent.SDL_KEYDOWN) {
+				// On génère un nouvel événement de type SDLKeyboardEvent - Attention au cast !
+				SDLKeyboardEvent eventK = (SDLKeyboardEvent) event;
+				
+				// Les méthodes getType() et getSym() se ressemblent sur le principe d'utilisation
+				switch (eventK.getSym()) {
+				
+				// Si la touche espace est pressée 
+				case SDLKey.SDLK_ESCAPE: running = false;break;
+				}
+			}
+			
+			image.blitSurface(screen, imagePos);
+			tank.blitSurface(screen, tankPos);
+			screen.flip();
+		}
+		
+		
+
+		// On quitte :: Libération de la mémoire
+		tank.freeSurface();
+		image.freeSurface();
 		screen.freeSurface();
 		SDLMain.quit();
 	}
